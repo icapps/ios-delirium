101cb4ece01e068402c1bf669f08c7c7a88260c5
Jelle Vandebeeck <jelle@fousa.be>
2016-06-10 12:09:06 +0200

    Add a working test.

---
+++ Example/Tests/EmptySpec.swift
@@ -0,0 +1,16 @@
+//
+//  EmptySpec.swift
+//  Delirium
+//
+//  Created by Jelle Vandebeeck on 10/06/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class EmptySpec: QuickSpec {
+    override func spec() {
+        
+    }
+}
