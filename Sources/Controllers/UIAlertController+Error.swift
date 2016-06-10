c6fd3890bb87499c547014be84ec8fd468174024
Jelle Vandebeeck <jelle@fousa.be>
2016-06-10 16:58:56 +0200

    Add the Dutch localization.

--- Sources/Controllers/UIAlertController+Error.swift
+++ Sources/Controllers/UIAlertController+Error.swift
@@ -26,7 +26,7 @@ extension UIAlertController {
     ///
     /// - Parameter error: The `NSError` to display in the alert.
     /// - Parameter title: The title can be manually set and is 'An error occured' by default.
-    public convenience init(title: String = NSLocalizedString("delirium.alert.title", bundle: NSBundle.deliriumBundle(), comment: ""), error: NSError) {
+    public convenience init(title: String = NSLocalizedString("delirium.alert.title", bundle: NSBundle.localeBundle(), comment: ""), error: NSError) {
         self.init(title: title, message: error.localizedDescription, preferredStyle: .Alert)
     }
     
@@ -64,9 +64,9 @@ extension UIViewController {
     }
     
     private func presentAlertController(controller: UIAlertController, retry: (() -> ())? = nil) {
-        controller.addAction(UIAlertAction(title: NSLocalizedString("delirium.alert.button.ok", bundle: NSBundle.deliriumBundle(), comment: ""), style: .Cancel, handler: nil))
+        controller.addAction(UIAlertAction(title: NSLocalizedString("delirium.alert.button.ok", bundle: NSBundle.localeBundle(), comment: ""), style: .Cancel, handler: nil))
         if let retry = retry {
-            controller.addAction(UIAlertAction(title: NSLocalizedString("delirium.alert.button.tryagain", bundle: NSBundle.deliriumBundle(), comment: ""), style: .Default) { action in
+            controller.addAction(UIAlertAction(title: NSLocalizedString("delirium.alert.button.tryagain", bundle: NSBundle.localeBundle(), comment: ""), style: .Default) { action in
                 retry()
             })
         }
--- Sources/Helpers/NSBundle+Cocoapods.swift
+++ Sources/Helpers/NSBundle+Cocoapods.swift
@@ -17,4 +17,15 @@ extension NSBundle {
         return NSBundle(path: bundlePath)!
     }
     
+    internal static func localeBundle() -> NSBundle {
+        // Fetch your current language.
+        var components = NSLocale.componentsFromLocaleIdentifier(NSLocale.currentLocale().localeIdentifier)
+        var language = components[NSLocaleLanguageCode]
+        
+        // When we can't find the bundle we prepare the English bunde.
+        var path = deliriumBundle().pathForResource(language, ofType: "lproj")
+        if path == nil { path = deliriumBundle().pathForResource("en", ofType: "lproj") }
+        return NSBundle(path: path!)!
+    }
+    
 }
