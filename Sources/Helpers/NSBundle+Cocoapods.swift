1bac3e66d64f576381489b0be0d59c23269eaf9a
Jelle Vandebeeck <jelle@fousa.be>
2016-06-10 17:14:00 +0200

    Convert var to let.

--- Sources/Helpers/NSBundle+Cocoapods.swift
+++ Sources/Helpers/NSBundle+Cocoapods.swift
@@ -19,8 +19,8 @@ extension NSBundle {
     
     internal static func localeBundle() -> NSBundle {
         // Fetch your current language.
-        var components = NSLocale.componentsFromLocaleIdentifier(NSLocale.currentLocale().localeIdentifier)
-        var language = components[NSLocaleLanguageCode]
+        let components = NSLocale.componentsFromLocaleIdentifier(NSLocale.currentLocale().localeIdentifier)
+        let language = components[NSLocaleLanguageCode]
         
         // When we can't find the bundle we prepare the English bunde.
         var path = deliriumBundle().pathForResource(language, ofType: "lproj")
