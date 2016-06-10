513fae323c5d25698b09658a474a600cf1d5c649
Jelle Vandebeeck <jelle@fousa.be>
2016-06-10 12:34:06 +0200

    Setup the tvOS view and organize the project structure.

---
+++ Example/Example for tvOS Delirium/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Delirium_tvExample
+//
+//  Created by Jelle Vandebeeck on 10/06/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+    
+}
