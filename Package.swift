679ab62e9218a8c2a809f2d4a5a0c44b14c9936c
Jelle Vandebeeck <jelle@fousa.be>
2016-06-10 11:17:45 +0200

    Add a swift package.

---
+++ Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+  name: "Delirium",
+  dependencies: [
+    .Package(url: "https://github.com/icapps/ios-delirium.git", majorVersion: 0, minor: 1)
+  ]
+)
