e6ebb38cc8588fb3314b8839bae934f2881eb2d9
Jelle Vandebeeck <jelle@fousa.be>
2016-06-10 12:30:17 +0200

    Add a new tvOS target

---
+++ Example/Pods/Target Support Files/Pods-Delirium-Delirium_Example/Pods-Delirium-Delirium_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Delirium_Delirium_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Delirium_Delirium_ExampleVersionString[];
+
--- Example/Pods/Target Support Files/Pods-Delirium-Delirium_tvExample/Pods-Delirium-Delirium_tvExample-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-Delirium-Delirium_tvExample/Pods-Delirium-Delirium_tvExample-frameworks.sh
@@ -84,8 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/Delirium/Delirium.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Delirium-tvOS/Delirium.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/Delirium/Delirium.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Delirium-tvOS/Delirium.framework"
 fi
