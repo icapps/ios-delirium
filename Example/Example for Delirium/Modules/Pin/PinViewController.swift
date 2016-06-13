4b62348966077b107e72e009451e2fc408db5e0c
Jelle Vandebeeck <jelle@fousa.be>
2016-06-13 10:40:56 +0200

    Prepare the pin view layout.

---
+++ Example/Example for Delirium/Modules/Pin/PinViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PinViewController.swift
+//  Delirium
+//
+//  Created by Jelle Vandebeeck on 13/06/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class PinViewController: UIViewController {
+    
+    // MARK: - Actions
+    
+    @IBAction func setupPin(sender: AnyObject) {
+    }
+    
+    @IBAction func verifyPin(sender: AnyObject) {
+    }
+    
+}
