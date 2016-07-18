78f963fdb9b4e1ab943bdc10922f8ecd9f101f7d
Jelle Vandebeeck <jelle@fousa.be>
2016-07-18 15:46:08 +0200

    Prepare test view.

---
+++ Example/Example for Delirium/Modules/Blurred Transition/BlurredTransitionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BlurredTransitionViewController.swift
+//  Delirium
+//
+//  Created by Jelle Vandebeeck on 18/07/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class BlurredTransitionViewController: UIViewController {
+    
+    // MARK: - View flow
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Blurred Transition"
+    }
+    
+}
