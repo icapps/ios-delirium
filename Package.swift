d7abbc9b012053cce7916ff9ee444eacd71957b8
Jelle Vandebeeck <jelle@fousa.be>
2016-07-20 16:37:33 +0200

    Bump the version.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name: "Delirium",
   dependencies: [
-    .Package(url: "https://github.com/icapps/ios-delirium.git", majorVersion: 0, minor: 2)
+    .Package(url: "https://github.com/icapps/ios-delirium.git", majorVersion: 0, minor: 4)
   ]
 )
--- README.md
+++ README.md
@@ -27,7 +27,7 @@
 Delirium is available through [CocoaPods](http://cocoapods.org). To install it, simply add the following line to your `Podfile`:
 
 ```ruby
-pod 'Delirium', '~> 0.3'
+pod 'Delirium', '~> 0.4'
 ```
 
 ## Features
