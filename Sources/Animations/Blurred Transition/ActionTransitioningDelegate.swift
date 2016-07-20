4e2cd0e8d8af06003a3b7ee8c1551e75c69abd38
Jelle Vandebeeck <jelle@fousa.be>
2016-07-20 16:36:12 +0200

    Add some inline documentation to the code.

--- Sources/Animations/Blurred Transition/ActionTransitioningDelegate.swift
+++ Sources/Animations/Blurred Transition/ActionTransitioningDelegate.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
+/// Present a `UIViewController` as a modal with a blurred background.
+///
+/// Set the `transitioningDelegate` to an instance of `ActionTransitioningDelegate` and the `modalPresentationStyle` to .Custom. Now present the `UIViewController` as a modal and boom!
+///
+/// The size of the controller is defined by the `intrinsicContentSize` of the `view` belonging to the controller.
 @available(iOS 9, *)
 public class ActionTransitioningDelegate: NSObject, UIViewControllerTransitioningDelegate {
     
     // MARK: - Configuration
     
+    /// The blurred effect style of the background view can be set with this property.
+    ///
+    /// The possible values can be found in the `UIBlurEffectStyle` docs.
     public var blurEffectStyle: UIBlurEffectStyle = .Light
     
     // MARK: - UIViewControllerTransitioningDelegate
