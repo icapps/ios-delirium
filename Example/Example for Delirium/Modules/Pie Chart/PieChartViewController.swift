6a36f28db488e89adc9f53c439eb44f3d316488f
Jelle Vandebeeck <jelle@fousa.be>
2016-08-23 15:44:44 +0200

    Add a simple pie chart example controller.

---
+++ Example/Example for Delirium/Modules/Pie Chart/PieChartViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PieChartViewController.swift
+//  Delirium
+//
+//  Created by Jelle Vandebeeck on 23/08/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class PieChartViewController: UIViewController {
+
+    // MARK: - View flow
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Pie Chart"
+    }
+
+}
