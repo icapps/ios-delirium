07d40acf8e93edb46b14b3d4f42b1cf674becacd
Jelle Vandebeeck <jelle@fousa.be>
2016-08-25 10:37:12 +0200

    Fix the pods for the tests.

--- Example/Pods/Target Support Files/Pods-Delirium-Delirium_Example/Pods-Delirium-Delirium_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-Delirium-Delirium_Example/Pods-Delirium-Delirium_Example-frameworks.sh
@@ -84,8 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/Delirium-iOS/Delirium.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Delirium-iOS8.0/Delirium.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/Delirium-iOS/Delirium.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Delirium-iOS8.0/Delirium.framework"
 fi
