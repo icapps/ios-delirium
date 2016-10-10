2ac03a11d67da4293c579b50880ba7b00cc0449c
StijnWillems <stijn.willems@icapps.com>
2016-10-10 21:58:26 +0200

    animates along the keyboard

--- Sources/Animations/KeyboardConstraint.swift
+++ Sources/Animations/KeyboardConstraint.swift
@@ -1,13 +1,12 @@
 import UIKit
 
-/// Set any NSLayoutConstraint class to a this class and it moves up and down with the keyboard.
+/// Set any NSLayoutConstraint class to this class and it moves up and down with the keyboard.
 open class KeyboardConstraint: NSLayoutConstraint {
-    /// this amount is added to the keyboard height
-    public var offsetFromKeyboardHeight: CGFloat = 0
+    /// this amount is added to the keyboard height to show the view above the keyboard.
+    public var offsetFromKeyboardHeight: CGFloat = 15
     /// will be set on awakeFromNib to the constant value. 
     /// Is used to move back to the position before the keyboard was shown
     public var normalConstant: CGFloat = 0
-    public static var animationDuration: TimeInterval = 0.7
 
     private var notifications: [Any]?  = [Any]()
 
@@ -18,10 +17,11 @@ open class KeyboardConstraint: NSLayoutConstraint {
                                                object: nil,
                                                queue: OperationQueue.main)
                     { [weak self] (notification) in
-                        let userInfo = notification.userInfo?[UIKeyboardFrameBeginUserInfoKey]
-                        if let keyboardSize = (userInfo as? NSValue)?.cgRectValue {
+                        let userInfo = notification.userInfo
+                        let frameInfo = userInfo?[UIKeyboardFrameBeginUserInfoKey]
+                        if let keyboardSize = (frameInfo as? NSValue)?.cgRectValue {
                             let offset = self?.offsetFromKeyboardHeight != nil ? self!.offsetFromKeyboardHeight : 0
-                            self?.animate(to: (keyboardSize.height + offset))
+                            self?.animate(to: (keyboardSize.height + offset), userInfo: userInfo)
                         }
                     }
 
@@ -29,7 +29,8 @@ open class KeyboardConstraint: NSLayoutConstraint {
                                                           object: nil,
                                                           queue: OperationQueue.main)
                     { [weak self] (notification) in
-                        self?.animate(to: self?.normalConstant)
+                        let userInfo = notification.userInfo
+                        self?.animate(to: self?.normalConstant, userInfo: userInfo)
                     }
 
         notifications?.append(not1)
@@ -37,20 +38,23 @@ open class KeyboardConstraint: NSLayoutConstraint {
         super.awakeFromNib()
     }
 
-    private func animate(to newConstant: CGFloat?) {
-        guard let newConstant = newConstant else {
+    private func animate(to newConstant: CGFloat?, userInfo: [AnyHashable : Any]?) {
+        guard let newConstant = newConstant,
+            let duration = (userInfo?[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue,
+            let curve = (userInfo?[UIKeyboardAnimationCurveUserInfoKey] as? NSNumber)?.uintValue else {
             return
         }
 
         self.constant = newConstant
-        UIView.animate(withDuration: KeyboardConstraint.animationDuration) {
+        UIView.animate(withDuration: duration, delay: 0, options: UIViewAnimationOptions(rawValue: curve), animations: {
             guard let view = self.firstItem as? UIView,
                 let view2 = self.firstItem as? UIView else {
                     return
             }
-            view.layoutIfNeeded()
-            view2.layoutIfNeeded()
-        }
+            view.superview?.layoutIfNeeded()
+            view2.superview?.layoutIfNeeded()
+        }, completion: nil)
+
     }
     deinit {
         guard let notifications = notifications else {
