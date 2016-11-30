c79e0cc242e05e449c9f1857c514bc7c17c6884c
StijnWillems <stijn.willems@icapps.com>
2016-11-30 12:56:23 +0100

    refactor for readability

--- Sources/StickeyCollectionViewLayout/StickyCollectionViewLayout.swift
+++ Sources/StickeyCollectionViewLayout/StickyCollectionViewLayout.swift
@@ -9,95 +9,19 @@ public class StickyCollectionViewLayout: UICollectionViewLayout {
     override public func prepare() {
         super.prepare()
         guard let collectionView = collectionView,
-              let sectionCount = self.collectionView?.numberOfSections,
-              sectionCount > 0 else {
+              let numberOfSections = self.collectionView?.numberOfSections,
+              numberOfSections > 0 else {
             return
-        }
-        
-        if itemAttributes.count > 0 {
-            for section in 0..<self.collectionView!.numberOfSections {
-                let numberOfItems : Int = self.collectionView!.numberOfItems(inSection: section)
-                for index in 0..<numberOfItems {
-                    if section != 0 && index != 0 {
-                        continue
-                    }
-                    
-                    let attributes = self.layoutAttributesForItem(at: IndexPath(item: index, section: section))!
-                    if section == 0 {
-                        var frame = attributes.frame
-                        frame.origin.y = self.collectionView!.contentOffset.y
-                        attributes.frame = frame
-                    }
-                    
-                    if index == 0 {
-                        var frame = attributes.frame
-                        frame.origin.x = self.collectionView!.contentOffset.x
-                        attributes.frame = frame
-                    }
-                }
-            }
-            return
-        }
-        
-        self.calculateItemsSize()
-        
-        var column = 0
-        var xOffset : CGFloat = 0
-        var yOffset : CGFloat = 0
-        var contentWidth : CGFloat = 0
-        var contentHeight : CGFloat = 0
-        
-        for section in 0..<self.collectionView!.numberOfSections {
-            var rowAttributes = [UICollectionViewLayoutAttributes]()
-            let numberOfColumns = collectionView.numberOfItems(inSection: section)
-            for index in 0..<numberOfColumns {
-                let itemSize = (self.itemsSize[index] as AnyObject).cgSizeValue
-                let indexPath = IndexPath(item: index, section: section)
-                let rowAttribute = UICollectionViewLayoutAttributes(forCellWith: indexPath)
-                rowAttribute.frame = CGRect(x: xOffset, y: yOffset, width: (itemSize?.width)!, height: (itemSize?.height)!).integral
-                
-                if section == 0 && index == 0 {
-                    rowAttribute.zIndex = 1024;
-                } else  if section == 0 || index == 0 {
-                    rowAttribute.zIndex = 1023
-                }
-                
-                if section == 0 {
-                    var frame = rowAttribute.frame
-                    frame.origin.y = self.collectionView!.contentOffset.y
-                    rowAttribute.frame = frame
-                }
-                if index == 0 {
-                    var frame = rowAttribute.frame
-                    frame.origin.x = self.collectionView!.contentOffset.x
-                    rowAttribute.frame = frame
-                }
-                
-               rowAttributes.append(rowAttribute)
-                
-                xOffset += (itemSize?.width)!
-                column += 1
-                
-                if column == numberOfColumns {
-                    if xOffset > contentWidth {
-                        contentWidth = xOffset
-                    }
-                    
-                    column = 0
-                    xOffset = 0
-                    yOffset += (itemSize?.height)!
-                }
-            }
-            self.itemAttributes.append(rowAttributes)
         }
 
-        guard let lastAttribute = itemAttributes.last?.last else {
+        guard itemAttributes.count > 0 else  {
+            // Calculate everything at startup. This code is only executed once
+            calculateAllItemAtrributes()
             return
         }
-        contentHeight = lastAttribute.frame.origin.y + lastAttribute.frame.size.height
-        self.contentSize = CGSize(width: contentWidth, height: contentHeight)
+        stickColumnHeaderAndFirstColumn()
     }
-    
+
     override public var collectionViewContentSize : CGSize {
         return self.contentSize
     }
@@ -120,11 +44,14 @@ public class StickyCollectionViewLayout: UICollectionViewLayout {
         return true
     }
     
-    func sizeForItemWithColumnIndex(_ columnIndex: Int) -> CGSize {
+
+    // MARK: - Privates
+
+    private func sizeForItemWithColumnIndex(_ columnIndex: Int) -> CGSize {
         return CGSize(width: 150, height: 40)
     }
     
-    func calculateItemsSize() {
+    private func calculateItemsSize() {
         guard let collectionView = collectionView else {
             return
         }
@@ -133,5 +60,113 @@ public class StickyCollectionViewLayout: UICollectionViewLayout {
                 self.itemsSize.append(self.sizeForItemWithColumnIndex(index))
             }
         }
+    }
+
+    private func stickColumnHeaderAndFirstColumn() {
+        guard let collectionView = collectionView,
+            let numberOfSections = self.collectionView?.numberOfSections,
+            numberOfSections > 0 else {
+                return
+        }
+
+        let contentOffset = collectionView.contentOffset
+        for section in 0..<numberOfSections {
+            let numberOfItems : Int = collectionView.numberOfItems(inSection: section)
+            for index in 0..<numberOfItems {
+                if section != 0 && index != 0 {
+                    continue
+                }
+
+                let attributes = self.layoutAttributesForItem(at: IndexPath(item: index, section: section))!
+
+                // Stick the first row
+                if section == 0 {
+                    var frame = attributes.frame
+                    frame.origin.y = contentOffset.y
+                    attributes.frame = frame
+                }
+
+                // Stick the first column
+                if index == 0 {
+                    var frame = attributes.frame
+                    frame.origin.x = contentOffset.x
+                    attributes.frame = frame
+                }
+            }
+        }
+    }
+
+    private func calculateAllItemAtrributes() {
+
+        guard let collectionView = collectionView,
+            let numberOfSections = self.collectionView?.numberOfSections,
+            numberOfSections > 0 else {
+                return
+        }
+
+        self.calculateItemsSize()
+
+        var column = 0
+        var xOffset : CGFloat = 0
+        var yOffset : CGFloat = 0
+        var contentWidth : CGFloat = 0
+        var contentHeight : CGFloat = 0
+
+        for section in 0..<self.collectionView!.numberOfSections {
+            var rowAttributes = [UICollectionViewLayoutAttributes]()
+            let numberOfColumns = collectionView.numberOfItems(inSection: section)
+            let contentOffset = collectionView.contentOffset
+
+            for index in 0..<numberOfColumns {
+                let itemSize = (self.itemsSize[index] as AnyObject).cgSizeValue
+                let indexPath = IndexPath(item: index, section: section)
+                let rowAttribute = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+                rowAttribute.frame = CGRect(x: xOffset, y: yOffset, width: (itemSize?.width)!, height: (itemSize?.height)!).integral
+
+                if section == 0 && index == 0 {
+                    rowAttribute.zIndex = 1024;
+                } else  if section == 0 || index == 0 {
+                    rowAttribute.zIndex = 1023
+                }
+
+                // Stick the first row
+                if section == 0 {
+                    var frame = rowAttribute.frame
+                    frame.origin.y = contentOffset.y
+                    rowAttribute.frame = frame
+                }
+
+                // Stick the first column
+
+                if index == 0 {
+                    var frame = rowAttribute.frame
+                    frame.origin.x = contentOffset.x
+                    rowAttribute.frame = frame
+                }
+
+                rowAttributes.append(rowAttribute)
+
+                xOffset += (itemSize?.width)!
+                column += 1
+
+                if column == numberOfColumns {
+                    if xOffset > contentWidth {
+                        contentWidth = xOffset
+                    }
+
+                    column = 0
+                    xOffset = 0
+                    yOffset += (itemSize?.height)!
+                }
+            }
+
+            self.itemAttributes.append(rowAttributes)
+        }
+
+        guard let lastAttribute = itemAttributes.last?.last else {
+            return
+        }
+        contentHeight = lastAttribute.frame.origin.y + lastAttribute.frame.size.height
+        self.contentSize = CGSize(width: contentWidth, height: contentHeight)
     }
 }
