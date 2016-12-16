5c9441a5e2d2480de4df14085936d4d444cfd7dd
StijnWillems <stijn.willems@icapps.com>
2016-12-16 11:59:17 +0100

    more cell to show functionality

--- Example/Example for Delirium/Modules/StickyCollectionViewLayout/StickyViewModel.swift
+++ Example/Example for Delirium/Modules/StickyCollectionViewLayout/StickyViewModel.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum Row: Int {
-    case row1, row2, row3, row4, row5, row6, row7, row8, row9, row10, row11, row12, row13, row14, row15, row16, row17, row18
+    case row1, row2, row3, row4, row5, row6, row7, row8, row9, row10, row11, row12, row13, row14, row15, row16, row17, row18, row19, row20, row21, row22, row23, row24, row25
 }
 
 enum Column: Int {
@@ -15,7 +15,9 @@ class ViewModel {
         let row1: [Column] = [.A, .B, .C, .D, .E, .F, .G, .H, .last]
         let row2: [Column] = [.I, .J, .K, .L, .M, .N, .O, .P, .last]
         let row3: [Column] = [.Q, .R, .S, .T, .U, .V, .W, .X, .last]
-        self.model = [.row1:row1, .row2:row2, .row3:row3, .row4:row1, .row5:row2, .row6:row3, .row7:row1, .row8: row2, .row9: row3, .row10: row1]
+        self.model = [.row1:row1, .row2:row2, .row3:row3, .row4:row1, .row5:row2, .row6:row3, .row7:row1, .row8: row2, .row9: row3, .row10: row1,
+                      .row11:row1, .row12:row2, .row13:row3, .row14:row1, .row15:row2, .row16:row3, .row17:row1, .row18: row2, .row19: row3, .row20: row1,
+                      .row21:row1, .row22:row2, .row23:row3, .row24:row1, .row25:row2]
     }
 
     var numberOfSection: Int {
